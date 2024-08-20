7c18bcbb4402828962b4651fc2f50d687ba7c83a
PathipatiMeghana <130462039+PathipatiMeghana@users.noreply.github.com>
2024-08-20 22:33:09 +0530

    Add files via upload

---
+++ armstrong.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    int num, originalNum, remainder;
+    int sum=0,n=0;
+    cout << "Enter an integer: ";
+    cin >> num;
+    originalNum=num;
+    while(originalNum!=0){
+    	originalNum/=10;
+    	n++;
+    }
+    originalNum=num;
+     while (originalNum != 0) {
+        remainder = originalNum % 10;
+        sum += pow(remainder, n);
+        originalNum /= 10;
+    }
+    if (sum == num)
+        cout << num << " is an Armstrong number." << endl;
+    else
+        cout << num << " is not an Armstrong number." << endl;
+
+    return 0;
+}
+    
+
+   
